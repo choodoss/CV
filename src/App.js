67bf1fbda4359e5fe0b2b5b2f1961de7492e8b9e
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2023-07-02 14:19:59 +0300

    create CV

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<CvLayout />} >
+          <Route index element={<Home />} />
+          {/* <Route path="/move" element={<Move />} />
+          <Route path="/move/:filmId" element={<MoveDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
+    </>
   );
 }
 
