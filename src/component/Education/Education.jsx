b3629a9694956fd1f7c1af31b35c7cd15f4dc966
Marina <82578557+choodoss@users.noreply.github.com>
2024-01-04 15:31:14 +0200

    fix data in degree

--- src/component/Education/Education.jsx
+++ src/component/Education/Education.jsx
@@ -16,12 +16,12 @@ const Education = () => {
                 <EduBox>
                     <Title>Odessa National Academy of Telecommunications n.a. O.S. Popov</Title>
                     <Description>master's degree, <DescriptionSpan>Telecommunication systems and networks</DescriptionSpan></Description>
-                    <DateTime>2006-2008</DateTime>
+                    <DateTime>2011-2012</DateTime>
                 </EduBox>
                 <EduBox>
                     <Title>Odessa National Academy of Telecommunications n.a. O.S. Popov</Title>
                     <Description>bachelor degree, <DescriptionSpan>Telecommunication systems and networks</DescriptionSpan></Description>
-                    <DateTime>2004-2006</DateTime>
+                    <DateTime>2008-2011</DateTime>
                 </EduBox>
                 <EduBox>
                     <Title>Kyiv College of Communication</Title>
